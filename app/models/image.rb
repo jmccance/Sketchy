a1ea0a610f659f41c99c027ed15406d8da8fe23c
Joel McCance <joelm@slalom.com>
2015-02-25 10:30:49 -0600

    Fixed new sketch creation by defining a valid default PNG.

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,7 +2,9 @@ class Image
   include MongoMapper::EmbeddedDocument
 
   key :type,      String, :default => 'image/png'
-  key :data,      Binary, :default => ''
+  key :data,      Binary, :default => Base64.strict_decode64(
+      'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAAxJRE' +
+      'FUCB1j+P//PwAF/gL+n8otEwAAAABJRU5ErkJggg==')
 
   def to_data_url
     "data:#{self.type};base64,#{Base64.strict_encode64(self.data.to_s)}"
